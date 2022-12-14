ef2fa058a6db2760830d0eb1401193da76bbc42f
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-12-14 21:01:14 +0100

    Implementirana klasa za konekciju

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ba.unsa.etf.rpr.rprprojekat {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
 
     opens ba.unsa.etf.rpr.rprprojekat to javafx.fxml;
     exports ba.unsa.etf.rpr.rprprojekat;
