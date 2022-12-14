8d07dc45cb1563ef94a6d60cb8c8e6dd0dabceed
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-12-14 21:22:21 +0100

    login controller dodan

---
+++ src/main/java/ba/unsa/etf/rpr/rprprojekat/LoginController.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.rprprojekat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+    @FXML
+    protected void onloginButtonClick () {
+        
+    }
+
+}
