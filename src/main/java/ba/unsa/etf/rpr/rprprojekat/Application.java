1d8f407858ec6a6789952e5a4239b2859c27cecb
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-12-14 21:29:20 +0100

    uspjesno pokrenuta login forma

--- src/main/java/ba/unsa/etf/rpr/rprprojekat/Application.java
+++ src/main/java/ba/unsa/etf/rpr/rprprojekat/Application.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 260, 100);
+        stage.setTitle("Login!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ba/unsa/etf/rpr/rprprojekat/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/rprprojekat/LoginController.java
@@ -2,12 +2,14 @@ package ba.unsa.etf.rpr.rprprojekat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.InputMethodEvent;
 
 public class LoginController {
 
     @FXML
-    protected void onloginButtonClick () {
-        
+    protected void onLoginButtonClick () {
+
     }
+
 
 }
