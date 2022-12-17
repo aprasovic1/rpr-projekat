79099acb0085899ba18e383bb015f4fa5cbbe0b9
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-12-17 10:44:20 +0100

    popravke u Dao.java

--- src/main/java/ba/unsa/etf/rpr/rprprojekat/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/rprprojekat/dao/Dao.java
@@ -1,12 +1,14 @@
 package ba.unsa.etf.rpr.rprprojekat.dao;
 
+import ba.unsa.etf.rpr.rprprojekat.exceptions.myException;
+
 import java.util.List;
 
 public interface Dao<T> {
 
-    T getById(int id);
-    T add(T item);
-    T update(T item);
-    void delete(int id);
-    List<T> getAll();
+    T getById(int id)throws myException;
+    T add(T item)throws myException;
+    T update(T item)throws myException;
+    void delete(int id)throws myException;
+    List<T> getAll()throws myException;
 }
