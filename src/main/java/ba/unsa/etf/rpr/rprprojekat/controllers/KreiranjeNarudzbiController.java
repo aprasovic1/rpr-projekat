3092fbcbebb5ff92a4a861e65981807239da7ac7
aprasovic1 <aprasovic1@etf.unsa.ba>
2023-02-20 16:02:24 +0100

    Izrada Kontrollera za kreiranje narudzbe

--- src/main/java/ba/unsa/etf/rpr/rprprojekat/controllers/KreiranjeNarudzbiController.java
+++ src/main/java/ba/unsa/etf/rpr/rprprojekat/controllers/KreiranjeNarudzbiController.java
@@ -1,38 +1,59 @@
 package ba.unsa.etf.rpr.rprprojekat.controllers;
 
+import ba.unsa.etf.rpr.rprprojekat.dao.ArtikalDaoImpl;
 import ba.unsa.etf.rpr.rprprojekat.dao.NarudzbaDaoImpl;
 import ba.unsa.etf.rpr.rprprojekat.dao.StavkaNarudzbeDaoImpl;
+import ba.unsa.etf.rpr.rprprojekat.domain.Artikal;
 import ba.unsa.etf.rpr.rprprojekat.domain.Narudzba;
+import ba.unsa.etf.rpr.rprprojekat.domain.StavkaNarudzbe;
 import ba.unsa.etf.rpr.rprprojekat.exceptions.myException;
+import com.mysql.cj.log.Log;
+import javafx.beans.Observable;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class KreiranjeNarudzbiController implements Initializable {
-    public TableView<Narudzba> narudzbeTable;
-    public TableColumn<Integer, Narudzba> narudzbaIdKolona;
-    public TableColumn<Integer, Narudzba> korisnikIdKolona;
-    public TableColumn<Date, Narudzba> datumNarudzbeIdKolona;
-    public MenuButton menuButton;
-    public TextField idNarudzbeText;
-    public Button brisiNardzbuButton;
+    public TableView<Artikal> artikliTable;
+    public TableColumn artikalIdKolona;
+    public TableColumn artikalNazivKolona;
+    public TableColumn artikalCijenaKolona;
+    public TableColumn artikalKolicinaKolona;
 
+    public TableView<Artikal> odabraniArtikliTable;
+    public TableColumn odabraniArtikalIdKolona;
+    public TableColumn odabranaKolicinaKolona;
+
+    public MenuButton menuButton;
+    public MenuItem upravljanjeKupcimaMenuItem;
+    public MenuItem upravljanjeArtiklimaMenuItem;
+    public TextField idArtiklaText;
+    public TextField kolicinaText;
+    public Button dodajArtikalButton;
+    public Button kreirajNarudzbuButton;
     NarudzbaDaoImpl n = new NarudzbaDaoImpl();
-    private NarudzbaModel nModel=new NarudzbaModel();
+    StavkaNarudzbeDaoImpl sn=new StavkaNarudzbeDaoImpl();
+    ArtikalDaoImpl a=new ArtikalDaoImpl();
+    private ArtikalModel aModel=new ArtikalModel();
     public KreiranjeNarudzbiController() {
     }
-
 
 
 
@@ -40,16 +61,22 @@ public class KreiranjeNarudzbiController implements Initializable {
     @FXML
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        narudzbaIdKolona.setCellValueFactory(new PropertyValueFactory<>("id"));
-        korisnikIdKolona.setCellValueFactory(new PropertyValueFactory<>("korisnik_id"));
-        datumNarudzbeIdKolona.setCellValueFactory(new PropertyValueFactory<>("datum_narudzbe"));
+        artikalIdKolona.setCellValueFactory(new PropertyValueFactory<>("id"));
+        artikalNazivKolona.setCellValueFactory(new PropertyValueFactory<>("naziv_artikla"));
+        artikalCijenaKolona.setCellValueFactory(new PropertyValueFactory<>("cijena"));
+        artikalKolicinaKolona.setCellValueFactory(new PropertyValueFactory<>("kolicina"));
+
+        odabraniArtikalIdKolona.setCellValueFactory(new PropertyValueFactory<>("id"));
+        odabranaKolicinaKolona.setCellValueFactory(new PropertyValueFactory<>("kolicina"));
+        bindAzuriranje();
         try {
-            for (Narudzba nx : n.getAll()) {
-                System.out.println(nx.toString());
+            for (Artikal art : a.getAll()) {
+                System.out.println(art.toString());
             }
-            narudzbeTable.getItems().clear();
-            narudzbeTable.setItems(n.getAll());
-            narudzbeTable.refresh();
+
+            artikliTable.getItems().clear();
+            artikliTable.setItems(a.getAll());
+            artikliTable.refresh();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -57,12 +84,37 @@ public class KreiranjeNarudzbiController implements Initializable {
         }
     }
 
-    public void onBrisiNarzdzbuButtonPressed(ActionEvent actionEvent) throws myException {
-        idNarudzbeText.textProperty().bindBidirectional((Property<String>) nModel.id.asString());
-        new StavkaNarudzbeDaoImpl().delete(nModel.id.getValue());
-        n.delete(nModel.id.getValue());
-        idNarudzbeText.clear();
-        narudzbeTable.refresh();
+    ArrayList<Artikal> arrArt=new ArrayList<Artikal>() ;
+    Narudzba nar=new Narudzba();
+    StavkaNarudzbe stavka=new StavkaNarudzbe();
+    public void onDodajArtikalButtonPressed(ActionEvent actionEvent) throws myException {
+        System.out.println("ID:"+aModel.id.getValue()+"\n");
+        System.out.println("KOL:"+aModel.kolicina.getValue());
+        Artikal art =a.getById(aModel.id.getValue());
+        int id = LoginController.ID;
+        nar.setDatum_narudzbe(java.time.LocalDate.now());
+        nar.setKorisnik_id(id);
+try{
+if(art.getKolicina()<aModel.kolicina.getValue()) throw new myException("Na stanju nema toliko komada artikla! ");
+}catch (myException m){
+    new Alert(Alert.AlertType.NONE, m.getMessage(), ButtonType.OK).show();
+}
+        stavka.setArtikal_id(aModel.id.getValue());
+        stavka.setKolicina(aModel.kolicina.getValue());
+        arrArt.add(art);
+
+       odabraniArtikliTable.getItems().clear();
+        odabraniArtikliTable.setItems(FXCollections.observableList(arrArt));
+        odabraniArtikliTable.refresh();
+
+
+    }
+    public void onKreirajNarudzbuButtonPressed(ActionEvent actionEvent) throws myException {
+        n.add(nar,stavka);
+        odabraniArtikliTable.getItems().clear();
+        odabraniArtikliTable.refresh();
+        arrArt.clear();
+a.skiniSaStanja(aModel.id.getValue(),aModel.kolicina.getValue());
 
     }
     public void onUpravljanjeArtiklimaPressed(ActionEvent actionEvent) {
@@ -75,26 +127,34 @@ public class KreiranjeNarudzbiController implements Initializable {
         Stage stara = (Stage) menuButton.getScene().getWindow();
         stara.hide();
     }
-    public class NarudzbaModel {
-        public SimpleIntegerProperty id = new SimpleIntegerProperty(), korisnik_id = new SimpleIntegerProperty();
-        public SimpleObjectProperty<LocalDate> datum_narudzbe = new SimpleObjectProperty<LocalDate>();
+    public class ArtikalModel {
+        public SimpleIntegerProperty id = new SimpleIntegerProperty(),
+                cijena = new SimpleIntegerProperty(),
+                kolicina = new SimpleIntegerProperty();
+        public SimpleStringProperty naziv = new SimpleStringProperty();
 
-        public void fromNarudzba(Narudzba n) {
-            this.id.set(n.getId());
-            this.korisnik_id.set(n.getKorisnik_id());
-            this.datum_narudzbe.set(n.getDatum_narudzbe());
+        public void fromArtikal(Artikal a) {
+            this.id.set(a.getId());
+            this.cijena.set(a.getCijena());
+            this.kolicina.set(a.getCijena());
+            this.naziv.set(a.getNaziv_artikla());
         }
 
-        public Narudzba toNarudzba() {
-            Narudzba n = new Narudzba();
-            n.setId(this.id.getValue());
-            n.setKorisnik_id(this.korisnik_id.getValue());
-            n.setDatum_narudzbe(datum_narudzbe.getValue());
-            return n;
+        public Artikal toArtikal() {
+            Artikal a = new Artikal();
+            a.setId(this.id.getValue());
+            a.setCijena(this.cijena.getValue());
+            a.setKolicina(this.kolicina.getValue());
+            a.setNaziv_artikla(this.naziv.getValue());
+            return a;
         }
 
     }
+    private void bindAzuriranje() {
+        idArtiklaText.textProperty().bindBidirectional(aModel.id, new NumberStringConverter());
+        kolicinaText.textProperty().bindBidirectional(aModel.kolicina, new NumberStringConverter());
 
+    }
 
 }
 
