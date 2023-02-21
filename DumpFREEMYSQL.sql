0517bdff07960c475eb60f3f0a1ecc072ea02cb7
aprasovic1 <83813071+aprasovic1@users.noreply.github.com>
2023-02-21 10:51:58 +0100

    Add files via upload

---
+++ DumpFREEMYSQL.sql
@@ -0,0 +1,138 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: sql7.freemysqlhosting.net    Database: sql7582884
+-- ------------------------------------------------------
+-- Server version	5.5.62-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `artikal`
+--
+
+DROP TABLE IF EXISTS `artikal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artikal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naziv_artikla` varchar(255) NOT NULL,
+  `cijena` double NOT NULL,
+  `kolicina` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artikal`
+--
+
+LOCK TABLES `artikal` WRITE;
+/*!40000 ALTER TABLE `artikal` DISABLE KEYS */;
+INSERT INTO `artikal` VALUES (1,'patike',155,3),(2,'sveska',2,4),(3,'cokolada',5,10),(4,'majica',21,5),(5,'telefon',120,6),(6,'monitor',340,10),(8,'lokomotiva',1530,4),(9,'papuce',15,11);
+/*!40000 ALTER TABLE `artikal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `korisnik`
+--
+
+DROP TABLE IF EXISTS `korisnik`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(20) NOT NULL,
+  `prezime` varchar(20) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `pass` varchar(30) NOT NULL,
+  `jesteAdmin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `tip_id` (`jesteAdmin`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+LOCK TABLES `korisnik` WRITE;
+/*!40000 ALTER TABLE `korisnik` DISABLE KEYS */;
+INSERT INTO `korisnik` VALUES (1,'Kenan','Kenanovic','kenan1','1kenan1',0),(2,'Esad','Eskovic','esad1','2esad1',0),(3,'Stipe','Stipic','stipe1','3stipe1',0),(4,'Milan','Milanovic','milan1','4milan1',0),(5,'Asmir','Prasovic','asmirp','1asmirp',1),(6,'Meho','Mehic','meho1','2meho1',0);
+/*!40000 ALTER TABLE `korisnik` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `narudzba`
+--
+
+DROP TABLE IF EXISTS `narudzba`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `narudzba` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `korisnik_id` int(11) NOT NULL,
+  `datum_narudzbe` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nar_kor_fk_idx` (`korisnik_id`),
+  CONSTRAINT `nar_kor_fk` FOREIGN KEY (`korisnik_id`) REFERENCES `korisnik` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `narudzba`
+--
+
+LOCK TABLES `narudzba` WRITE;
+/*!40000 ALTER TABLE `narudzba` DISABLE KEYS */;
+INSERT INTO `narudzba` VALUES (2,2,'2022-07-12'),(15,5,'2023-02-20'),(16,5,'2023-02-20'),(17,5,'2023-02-20'),(18,5,'2023-02-20'),(19,5,'2023-02-20'),(20,5,'2023-02-20');
+/*!40000 ALTER TABLE `narudzba` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stavka_narudzbe`
+--
+
+DROP TABLE IF EXISTS `stavka_narudzbe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stavka_narudzbe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `narudzba_id` int(11) NOT NULL,
+  `artikal_id` int(11) NOT NULL,
+  `kolicina` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sn_art_fk_idx` (`artikal_id`),
+  KEY `sn_nar_fk_idx` (`narudzba_id`),
+  CONSTRAINT `sn_nar_fk` FOREIGN KEY (`narudzba_id`) REFERENCES `narudzba` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stavka_narudzbe`
+--
+
+LOCK TABLES `stavka_narudzbe` WRITE;
+/*!40000 ALTER TABLE `stavka_narudzbe` DISABLE KEYS */;
+INSERT INTO `stavka_narudzbe` VALUES (20,2,4,6),(21,2,5,1),(22,2,6,4);
+/*!40000 ALTER TABLE `stavka_narudzbe` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-21 10:48:37
